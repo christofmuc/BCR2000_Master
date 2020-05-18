653aefa54900bc03f48e8db0c144a818b9b4ca4c
cruc <christof.ruch@gmx.de>
2020-05-18 14:58:29 +0200

    Updating to new version of midikraft-base to make sure the BCR2000 package still works.

--- BCRMaster/MainComponent.cpp
+++ BCRMaster/MainComponent.cpp
@@ -149,7 +149,7 @@ void MainComponent::detectBCR()
 	MouseCursor::showWaitCursor();
 	std::vector<std::shared_ptr<midikraft::SimpleDiscoverableDevice>> devices;
 	devices.push_back(bcr_);
-	autodetector_.autoconfigure(devices);
+	autodetector_.autoconfigure(devices, nullptr);
 	refreshFromBCR();
 }
 
